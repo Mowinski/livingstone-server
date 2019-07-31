1c1d23d82e4b628cdfa838e585cf3017f45d70b9
Kamil Mówiński <kamil.mowinski@mirumee.com>
2019-07-31 06:59:49 +0200

    Add exempt to graphql endpoint

--- DrLivingstoneMobileServer/urls.py
+++ DrLivingstoneMobileServer/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^graphql$', GraphQLView.as_view(graphiql=True)),
+    url(r'^graphql$', csrf_exempt(GraphQLView.as_view(graphiql=True))),
 ]
